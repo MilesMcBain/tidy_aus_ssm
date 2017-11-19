a028629f253aaf8ecdac096bd393d16467337487
milesmcbain <miles.mcbain@gmail.com>
2017-11-19 12:37:47 +1000

    Rejig dataset to more analysis ready form.

--- tidy_participation.R
+++ tidy_participation.R
@@ -1,25 +1,19 @@
-library(readxl)
 library(tidyverse)
+library(readxl)
 library(readr)
 
 file_name = "australian_marriage_law_postal_survey_2017_-_participation_final.xls"
 
-params <-
-  list(
-    table_name = c("Table 5", "Table 6"),
-    table_gender = c("Male", "Female")
-)
-
 extract_participation_counts <- function(file_name, table_name, table_gender){
   
 areas <- 
-  readxl::read_excel(
-                   path = file_name,
-                   range = paste0(table_name,"!A8:A650"),
-                   col_names = "area") %>%
+  read_excel(
+      path = file_name,
+      range = paste0(table_name,"!A8:A650"),
+      col_names = "area") %>%
   filter(!grepl("Division|Australia$", area)) %>%
   mutate(area = gsub("\\([a-z]\\)","",area)) %>%
-  tidyr::drop_na()
+  drop_na()
 
 age_counts <- readxl::read_excel(
   path = file_name,
@@ -42,27 +36,38 @@ age_counts <- readxl::read_excel(
                 "85 years and over",
                 "Total for Gender")) %>%
   tidyr::drop_na() %>%
-  mutate(area = rep(areas$area, each=3), gender=table_gender) %>% 
+  mutate(area = rep(areas$area, each=3), gender=table_gender) %>%
+  filter(!grepl("Total", area)) %>%
   gather(key = "age", value = "count", -measure, -area, -gender)
 }
+
+params <-
+  list(
+    table_name = c("Table 5", "Table 6"),
+    table_gender = c("Male", "Female")
+)
 
 ssm_participation <-
   pmap(.l = params, 
      .f = extract_participation_counts,
      file_name = file_name) %>%
-  bind_rows() %>%
-  filter(!grepl("Total", area))
+  bind_rows() 
 
-# Fetch the current electorates
+
+# Fetch the current electorates to append to table
 current_electorates <- 
   read_html("http://www.aec.gov.au/profiles/") %>%
   rvest::html_nodes("table") %>%
   rvest::html_table() %>%
-  first()
+  first() %>%
+  select(`Electoral division`, State, `Area (sq km)`)
 
 ssm_participation_state <- 
   ssm_participation %>%
-  left_join(current_electorates, by = c("area" = "Electoral division"))
+  left_join(current_electorates, by = c("area" = "Electoral division")) 
 
-write_csv(ssm_participation_state
-          , path = "SSM_AUS_Participation.csv")
+# Final spread of participation measures to columns and write to csv
+ssm_participation_state %>%
+  spread(measure, count) %>%
+  write_csv(path = "SSM_AUS_Participation.csv")
+
